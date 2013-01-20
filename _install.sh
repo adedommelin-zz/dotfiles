f561c154c2dcd509a8dda2fb32be3c6ea12570db
Alexandre De Dommelin <adedommelin@tuxz.net>
2013-01-20 17:31:18 +0100

    Force existing files removal

--- _install.sh
+++ _install.sh
@@ -2,5 +2,6 @@
 
 for f in $(ls -1 ~/.dotfiles/ | grep -Ev '^[_|README\.md]');
 do
-  [ -L ~/.${f} ] || /bin/ln -s ~/.dotfiles/${f} ~/.${f}
+  rm -f ~/.${f}
+  /bin/ln -s ~/.dotfiles/${f} ~/.${f}
 done
